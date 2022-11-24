8f8f3b6424667775ea0caabb28ffac499ceeb192
huangweiwen <weiwen.wong90@gmail.com>
2022-11-24 14:40:40 +0800

    Initial Commit

---
+++ sdui/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  sdui
+//
+//  Created by 黄伟文 on 2022/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sdui/sduiApp.swift
@@ -0,0 +1,17 @@
+//
+//  sduiApp.swift
+//  sdui
+//
+//  Created by 黄伟文 on 2022/11/24.
+//
+
+import SwiftUI
+
+@main
+struct sduiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
